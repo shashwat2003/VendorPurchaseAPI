b1a948a5a0c30d0271b8da30835d100b5b953e9e
Shashwat <info.superpcstudio@gmail.com>
2022-10-27 12:34:15 +0530

    Added Role Model, initiated VendorApp

--- UserApp/models.py
+++ UserApp/models.py
@@ -1,7 +1,16 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 
 class User(AbstractUser):
-    pass
+    profile_photo = models.FileField(default=None, null=True)
+    role = models.ForeignKey("Role",
+                             default="user",
+                             on_delete=models.SET_NULL,
+                             null=True)
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=10, unique=True, primary_key=True)
