fc8eb615b8bff5d2b9248f5f9c02133019e451e4
Shashwat <info.superpcstudio@gmail.com>
2022-10-29 23:00:36 +0530

    Added Login API

--- UserApp/views.py
+++ UserApp/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.status import *
+from django.contrib.auth import authenticate, login
+from .models import *
 # Create your views here.
+
+
+class UserLoginView(APIView):
+
+    def get(self, request: Request):
+        if request.user.is_authenticated and request.user.is_active:
+            return Response({"verified": True})
+        else:
+            return Response({"verified": False})
+
+    def post(self, request: Request):
+        print(request.data.keys())
+        if "email" in request.data.keys():
+            user = User.objects.filter(
+                email=request.data.get("email").strip()).first()
+            if user is None:
+                return Response({"error": "Invalid Username or Password..."},
+                                status=HTTP_400_BAD_REQUEST)
+            username = user.username
+        else:
+            username = request.data.get("username")
+        print(username)
+        user = authenticate(username=username,
+                            password=request.data.get("password"))
+        if user is not None:
+            login(request, user)
+            return Response({"success": "Login Success!"})
+        else:
+            return Response({"error": "Invalid Username or Password..."},
+                            status=HTTP_400_BAD_REQUEST)
--- VendorPurchaseAPI/settings.py
+++ VendorPurchaseAPI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = constants.KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = not constants.PRODUCTION
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -44,12 +44,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'UserApp.middleware',
+    'UserApp.middleware.DisableSessionCSRF',
 ]
 
 ROOT_URLCONF = 'VendorPurchaseAPI.urls'
@@ -135,3 +136,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Custom Settings
 
 AUTH_USER_MODEL = 'UserApp.User'
+
+# CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:5173"]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+SESSION_COOKIE_SAMESITE = "None"
